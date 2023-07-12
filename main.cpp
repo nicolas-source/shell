78665de1af3f6e6eb2287bb627d90c4bb468de0d
nicolas-source <nzheng8@student.ubc.ca>
2023-07-12 01:25:40 -0700

    obtaining argv ending with NULL works

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <unistd.h>
+#include <vector>
+#include <string.h>
+#include <cstring>
+
 
 void shell_loop();
 
-void shell_launcher(char** arguments);
+void shell_launcher(char **arguments);
+
+std::vector<std::string> shell_parse(std::string user_input);
 
 int main() {
     printf("Starting\n");
@@ -23,24 +29,60 @@ void shell_loop() {
 
     int status;
 
+    std::string user_input;
+
     do {
         printf("> ");
-        std::cin >> input;
-        std::cout << input << std::endl;
+        std::getline(std::cin, user_input, '\n');
+//        user_input = "awe bwefa zzvv\n";
+        std::vector<std::string> parsed_array;
+        parsed_array = shell_parse(user_input);
 
-        char first = 'a', second = 'b';
+        for (int i = 0; i < parsed_array.size(); i++) {
+            printf("%s\n", parsed_array[i].c_str());
+        }
 
-        char* args_arry[] = {&first, &second};
-        char** pointer = &args_arry[0];
+        char *char_arry[parsed_array.size()+1];
 
-        shell_launcher(pointer);
+        for (int i = 0; i < parsed_array.size(); i++) {
+            int token_size = ((std::string) parsed_array[i].c_str()).size();
+            char_arry[i] = new char[token_size];
+            strcpy(char_arry[i], (char *) parsed_array[i].c_str());
+        }
+        char_arry[parsed_array.size()] = NULL;
+
+        char *ptr = (char *) &parsed_array[0];
+        char **pptr = (char **) &char_arry;
+
+        shell_launcher(pptr);
 
         status = strcmp("quit", input);
 
     } while (status);
 }
 
-void shell_launcher(char** arguments) {
+std::vector<std::string> shell_parse(std::string user_input) {
+    int BUF_SIZE = 64;
+    const char delim = ' ';
+    char **tokens = (char **) malloc(BUF_SIZE * sizeof(char *));
+
+    *tokens = (char *) user_input.data();
+
+    std::vector<std::string> token_vector;
+
+    char *token;
+    token = strtok(*tokens, &delim);
+    while (token != NULL) {
+//        printf("%s\n", token);
+        token_vector.emplace_back(token);
+//        printf("%s\n", token_vector[0].c_str());
+        token = strtok(NULL, &delim);
+
+    }
+    return token_vector;
+}
+
+void shell_launcher(char **arguments) {
     pid_t pid, wait_pid;
     int status;
 
@@ -48,23 +90,24 @@ void shell_launcher(char** arguments) {
     if (pid == 0) {
         printf("I'm a child process\n");
 
-        int execv_ret = execvp(arguments[0], arguments);
-        if (execv_ret == -1) {
-            perror("Error: ");
-        }
+//        int execv_ret = execvp(arguments[0], arguments);
+//        if (execv_ret == -1) {
+//            perror("Error: ");
+//        }
 
         std::cout << arguments << std::endl;
-        std::cout << *arguments[0] << std::endl;
-        std::cout << *arguments[1] << std::endl;
+        std::cout << arguments[0] << std::endl;
+        std::cout << arguments[1] << std::endl;
+        std::cout << sizeof(arguments) << std::endl;
         std::cout << sizeof(*arguments) << std::endl;
 
-    } else if (pid > 0){
+    } else if (pid > 0) {
         printf("I'm a parent process\n");
         do {
             wait_pid = waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
 
-    } else if (pid < 0){
+    } else if (pid < 0) {
         perror("Error: ");
     }
 }
